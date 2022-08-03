db7ae34295842e59184dd306b83ab6709244cd3e
Arif Mridha Pranto <mridhapranto647@gmail.com>
2022-08-03 16:37:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
+
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className='bg-yoo text-yoo'> Pranto Mridha.</p>
       </header>
     </div>
   );
